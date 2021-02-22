1f640f535dca44e4c11639dec5b156c4f0746904
jihad khorfan <spyshow86@gmail.com>
2021-02-22 10:50:28 +0200

    add precentage card component to machine data

--- src/components/MachineData/MachineData.component.jsx
+++ src/components/MachineData/MachineData.component.jsx
@@ -2,32 +2,58 @@ import React, { useRef } from "react";
 import { useFind } from "figbird";
 import { Skeleton } from "antd";
 import ReactEcharts from "echarts-for-react";
+import moment from "moment";
 
-const MachineData = ({ machine_name, machine_sensors, timeRange }) => {
+import PrecentageCard from "./../PrecentageCard/PrecentageCard.component";
+import "./MachineData.styles.css";
+
+const MachineData = ({
+  machine_name,
+  machine_type,
+  machine_sensors,
+  timeRange,
+}) => {
+  console.log(machine_name, machine_sensors, timeRange);
   const chartEl = useRef(null);
   const machineData = useFind("machine-data", {
     query: {
       machine_name: machine_name,
+      machine_type: machine_type,
       startDate: timeRange[0]._d,
+      oldStartDate: moment(timeRange[0]).subtract(
+        moment(timeRange[1]).diff(moment(timeRange[0])),
+        "milliseconds"
+      ),
+      oldEndDate: timeRange[0]._d,
       endDate: timeRange[1]._d,
       machine_sensors: machine_sensors,
     },
     realtime: "refetch",
+    fetchPolicy: "network-only",
   });
-  console.log(machineData);
+  // machineData : [chart data] , newPrecentage , oldPrecentage
+  console.log(machineData.status);
   if (machineData.status !== "success") {
     return <Skeleton active />;
   } else if (machineData.data.data || machineData.data.length === 0) {
     return <div>No data</div>;
   }
+  console.log(machineData.oldPrecentage, machineData.newPrecentage);
   return (
-    <ReactEcharts
-      id={machine_name}
-      ref={chartEl}
-      option={machineData.data[0]}
-      style={{ height: "600px", width: "100%" }}
-      opts={{ renderer: "svg" }}
-    />
+    <div>
+      <PrecentageCard
+        machine_name={machine_name}
+        newPrecentage={machineData.newPrecentage}
+        oldPrecentage={machineData.oldPrecentage}
+      />
+      <ReactEcharts
+        id={machine_name}
+        ref={chartEl}
+        option={machineData.data[0]}
+        style={{ height: "600px", width: "100%" }}
+        opts={{ renderer: "svg" }}
+      />
+    </div>
   );
 };
 
