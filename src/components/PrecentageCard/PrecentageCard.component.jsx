f46186f084ec7b7c193219f595d2328a4aea7765
jihad khorfan <spyshow86@gmail.com>
2021-02-25 09:11:23 +0200

    adding the preselected date precentage

--- src/components/PrecentageCard/PrecentageCard.component.jsx
+++ src/components/PrecentageCard/PrecentageCard.component.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { ArrowUpOutlined, ArrowDownOutlined } from "@ant-design/icons";
 
 import "./PrecentageCard.styles.css";
 
 const PrecentageCard = ({ machine_name, newPrecentage, oldPrecentage }) => {
+  const [infoShow, setInfoShow] = useState(false);
+
+  console.log("new" + newPrecentage, oldPrecentage);
   return (
     <p className="machine-precentage">
       <span className="machine-precentage-text">
@@ -16,6 +19,10 @@ const PrecentageCard = ({ machine_name, newPrecentage, oldPrecentage }) => {
         <ArrowUpOutlined style={{ fontSize: "1rem", color: "#3f8600" }} />
       )}{" "}
       <span className="machine-precentage-total">{newPrecentage}</span>{" "}
+      <span className="machine-info">
+        <span className="machine-precentage-total">{100 - oldPrecentage}</span>{" "}
+        <span className="machine-precentage-total">{oldPrecentage}</span>{" "}
+      </span>
     </p>
   );
 };
