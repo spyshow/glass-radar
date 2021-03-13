761a2e196df54daa9bb4e96a691b7d052a31bb22
jihad khorfan <spyshow86@gmail.com>
2021-03-13 15:48:45 +0200

    adding mold box component

---
+++ src/components/MoldBox/MoldBox.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function MoldBox() {
+  return (
+    <div
+      class="periodic-element social-media"
+      data-description="I'm only on here to reject invitations 😂"
+    >
+      <div class="periodic-element-inner">
+        <div class="title">Li</div>
+        <div class="description">LinkedIn</div>
+      </div>
+    </div>
+  );
+}
