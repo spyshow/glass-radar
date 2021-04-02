050b42e35d7f90d4c8b1b827a49c25e871d11e0d
jihad khorfan <spyshow86@gmail.com>
2021-04-03 00:51:10 +0300

    machine card complete

--- src/components/MachineCard/MachineCard.component.jsx
+++ src/components/MachineCard/MachineCard.component.jsx
@@ -1,95 +1,60 @@
 import React, { useRef } from "react";
-import { Card, Divider } from "antd";
-import * as echarts from "echarts/core";
+import { Card, Divider, Skeleton } from "antd";
 import ReactEChartsCore from "echarts-for-react/lib/core";
-import { TooltipComponent, GridComponent } from "echarts/components";
+import * as echarts from "echarts/core";
+import {
+  ToolboxComponent,
+  TooltipComponent,
+  GridComponent,
+  DataZoomComponent,
+  DataZoomInsideComponent,
+} from "echarts/components";
 import { LineChart } from "echarts/charts";
 import { CanvasRenderer } from "echarts/renderers";
+import { ArrowUpOutlined, ArrowDownOutlined } from "@ant-design/icons";
+
 import { useGet } from "figbird";
 import moment from "moment";
 
 import "./MachineCard.styles.css";
 
-echarts.use([TooltipComponent, GridComponent, LineChart, CanvasRenderer]);
+echarts.use([
+  ToolboxComponent,
+  TooltipComponent,
+  GridComponent,
+  LineChart,
+  CanvasRenderer,
+  DataZoomComponent,
+  DataZoomInsideComponent,
+]);
 
-const MachineCard = ({ machine_id, option }) => {
+const MachineCard = ({ lineId, machineId, machineName, index }) => {
   const { Meta } = Card;
   const chartEl = useRef(null);
-  const newDate = moment().add(1, "hours").hour();
   const oldDate = moment().subtract(5, "hours").hour();
-  console.log(newDate, oldDate);
-  const machineCard = useGet("machine-card", machine_id, {
+  const machineCard = useGet("machine-card", machineId, {
     query: {
-      machine_id: machine_id,
-      newDate: newDate,
+      machineId: machineId,
+      machine_name: machineName,
+      lineId: lineId,
       oldDate: oldDate,
+      index: index,
+      echarts: echarts,
     },
     realtime: "refetch",
     fetchPolicy: "network-only",
   });
-
-  option = option = {
-    color: ["#80FFA5", "#00DDFF", "#37A2FF", "#FF0087", "#FFBF00"],
-
-    tooltip: {
-      trigger: "axis",
-      axisPointer: {
-        type: "cross",
-        label: {
-          backgroundColor: "#6a7985",
-        },
-      },
-    },
-
-    grid: {
-      top: "3%",
-      left: "3%",
-      right: "4%",
-      bottom: "3%",
-      containLabel: false,
-    },
-    xAxis: [
-      {
-        type: "category",
-        boundaryGap: false,
-      },
-    ],
-    yAxis: [
-      {
-        type: "value",
-        show: false,
-      },
-    ],
-    series: [
-      {
-        name: "Line 1",
-        type: "line",
-        stack: "总量",
-        smooth: true,
-        lineStyle: {
-          width: 0,
-        },
-        showSymbol: false,
-        areaStyle: {
-          opacity: 0.8,
-          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-            {
-              offset: 0,
-              color: "rgba(128, 255, 165)",
-            },
-            {
-              offset: 1,
-              color: "rgba(1, 191, 236)",
-            },
-          ]),
-        },
-        emphasis: {
-          focus: "series",
-        },
-        data: [140, 232, 101, 264, 90, 340, 250],
-      },
-    ],
-  };
+  console.log(machineCard.status);
+  if (machineCard.status !== "success") {
+    return <Skeleton active />;
+  } else if (
+    machineCard.data.data ||
+    machineCard.data.length === 0 ||
+    machineCard.data === undefined
+  ) {
+    return <div>No data</div>;
+  }
+  console.log(machineCard.data);
 
   return (
     <Card
@@ -104,15 +69,24 @@ const MachineCard = ({ machine_id, option }) => {
     >
       <Meta className="card" />
       <div>
-        <h2>MX4 M22</h2> <h1>92.1 ⬆ 7.9</h1>
+        <h2>{machineName}</h2>{" "}
+        <h1>
+          {machineCard.data.accepted}{" "}
+          {machineCard.data.percentage === "down" ? (
+            <ArrowDownOutlined style={{ fontSize: "1rem", color: "#cf1322" }} />
+          ) : (
+            <ArrowUpOutlined style={{ fontSize: "1rem", color: "#3f8600" }} />
+          )}{" "}
+          {machineCard.data.rejected}
+        </h1>
       </div>
       <Divider style={{ height: "70px" }} type="vertical" />
       <ReactEChartsCore
         echarts={echarts}
-        id={machine_id}
+        id={machineId}
         ref={chartEl}
         replaceMerge={["xAxis", "yAxis", "series"]}
-        option={option}
+        option={machineCard.data.option}
         style={{ height: "70px", width: "150px", display: "inline-block" }}
         opts={{ renderer: "canvas" }}
         notMerge={true}
