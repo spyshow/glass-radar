a5c44b8b7a2142f65c48f242c5fd1e0f4f0968b8
jihad khorfan <spyshow86@gmail.com>
2021-04-03 00:51:36 +0300

    adjust to show machine cards by each other

--- src/pages/Operator/Operator.page.jsx
+++ src/pages/Operator/Operator.page.jsx
@@ -1,22 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import useInterval from "@use-it/interval";
-//import { useGet } from "figbird";
+import { useFind } from "figbird";
 import axios from "axios";
+import { Skeleton } from "antd";
 import feathers from "@feathersjs/feathers";
 import rest from "@feathersjs/rest-client";
-import { Result, Skeleton } from "antd";
+import { Typography } from "antd";
+
+import MachineCard from "../../components/MachineCard/MachineCard.component";
 import MoldBox from "../../components/MoldBox/MoldBox.component";
 import "./Opertator.styles.css";
+
 //import axios from "axios";
+
 const app = feathers();
+const { Title } = Typography;
 const restClient = rest();
 app.configure(restClient.axios(axios));
+
 export default function Operator() {
-  let { id } = useParams();
+  let { machineId, lineId } = useParams();
+  const machines = useFind("machines", {
+    query: {
+      lineId: lineId,
+      $sort: {
+        sequence: 1,
+      },
+    },
+  });
+
   const [moldData, setMoldData] = useState({
     data: { mountedMolds: [], rejectedMolds: [] },
   });
+
   // const molds = useGet("molds", id);
   // setMoldData((previousState) => ({
   //   ...previousState,
@@ -25,7 +42,7 @@ export default function Operator() {
   function getMolds() {
     axios({
       method: "get",
-      url: process.env.REACT_APP_HOSTNAME + "/molds/" + id,
+      url: process.env.REACT_APP_HOSTNAME + "/molds/" + machineId,
     }).then((result) => {
       setMoldData((previousState) => ({
         ...previousState.data,
@@ -34,6 +51,7 @@ export default function Operator() {
       console.log(moldData);
     });
   }
+
   useInterval(() => {
     getMolds();
   }, 10000);
@@ -49,12 +67,31 @@ export default function Operator() {
       }
     }
   }
+  if (machines.status !== "success") {
+    return <Skeleton active />;
+  }
+  console.log(machines.data);
   console.log(finishedData);
   return (
-    <div className="periodic-table">
-      {finishedData.map((mold, i) => (
-        <MoldBox number={mold.number} status={mold.status} key={i} />
-      ))}
+    <div>
+      <Title level={2}>Operator Panel</Title>
+      <div className="cards">
+        {machines.data.map((machine, i) => (
+          <MachineCard
+            key={lineId + "_" + machine.machine_name}
+            machineName={machine.machine_name}
+            machineId={machine.id}
+            lineId={lineId}
+            index={i}
+          />
+        ))}
+      </div>
+
+      <div className="periodic-table">
+        {finishedData.map((mold, i) => (
+          <MoldBox number={mold.number} status={mold.status} key={i} />
+        ))}
+      </div>
     </div>
   );
 }
