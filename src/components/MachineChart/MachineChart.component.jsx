c8921c3f9e306bdf46c4840c53c0353fc9e9c5d2
jihad khorfan <spyshow86@gmail.com>
2021-08-16 08:46:29 +0200

    updating component

--- src/components/MachineChart/MachineChart.component.jsx
+++ src/components/MachineChart/MachineChart.component.jsx
@@ -1,12 +1,39 @@
 import React, { useRef } from "react";
 import { Card, Divider, Skeleton } from "antd";
+import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
+import ReactEChartsCore from "echarts-for-react/lib/core";
+import * as echarts from "echarts/core";
+import { useGet } from "figbird";
 
 import "./MachineChart.styles.css";
 
 const { Meta } = Card;
-const chartEl = useRef(null);
 
-const MachineChart = ({ option }) => {
+const MachineChart = ({ key, machineName, machineId, lineId, index }) => {
+  const chartEl = useRef(null);
+  const machineCard = useGet("machine-card", machineId, {
+    query: {
+      machineId: machineId,
+      machine_name: machineName,
+      lineId: lineId,
+      index: index,
+      echarts: echarts,
+    },
+    realtime: "refetch",
+    fetchPolicy: "network-only",
+  });
+  console.log(machineCard.status);
+  if (machineCard.status !== "success") {
+    return <Skeleton active />;
+  } else if (
+    machineCard.data.data ||
+    machineCard.data.length === 0 ||
+    machineCard.data === undefined
+  ) {
+    return <div>No data</div>;
+  }
+  console.log(machineCard.data);
+
   return (
     <Card
       hoverable
@@ -20,10 +47,10 @@ const MachineChart = ({ option }) => {
     >
       <Meta className="card" />
       <div>
-        <h2>{option.id}</h2>{" "}
+        <h2>{machineName}</h2>{" "}
         <h1>
-          {option.data.accepted}{" "}
-          {option.data.percentage === "down" ? (
+          {machineCard.data.accepted}{" "}
+          {machineCard.data.percentage === "down" ? (
             <ArrowDownOutlined style={{ fontSize: "1rem", color: "#cf1322" }} />
           ) : (
             <ArrowUpOutlined style={{ fontSize: "1rem", color: "#3f8600" }} />
@@ -34,10 +61,10 @@ const MachineChart = ({ option }) => {
       <Divider style={{ height: "70px" }} type="vertical" />
       <ReactEChartsCore
         echarts={echarts}
-        id={option.id}
+        id={machineCard.data.id}
         ref={chartEl}
         replaceMerge={["xAxis", "yAxis", "series"]}
-        option={option}
+        option={machineCard.data.option}
         style={{ height: "70px", width: "150px", display: "inline-block" }}
         opts={{ renderer: "canvas" }}
         notMerge={true}
