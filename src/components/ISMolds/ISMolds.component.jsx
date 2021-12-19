6c7172b798df2360f8991689a957276c742670cc
jihad khorfan <spyshow86@gmail.com>
2021-12-19 15:43:29 +0200

    add ISmolds components

---
+++ src/components/ISMolds/ISMolds.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Row, Col, Divider } from "antd";
+
+import "./ISMolds.styles.css";
+
+export default function ISMolds() {
+  return (
+    <div>
+      <Col className="gutter-row" span={6}>
+        <div>Blank</div>
+      </Col>
+    </div>
+  );
+}
