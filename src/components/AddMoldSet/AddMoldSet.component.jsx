783c5412fa9f6de12d2e95ce0d55d10b936ac04d
jihad khorfan <spyshow86@gmail.com>
2021-12-19 15:40:33 +0200

    add addmoldset component

---
+++ src/components/AddMoldSet/AddMoldSet.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './AddMoldSet.component.css'
+
+const AddMoldSet= () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AddMoldSet
