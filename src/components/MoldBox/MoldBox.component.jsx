38e66353606faa58e5ff4fc88d0305d1c0d429cf
jihad khorfan <spyshow86@gmail.com>
2022-05-11 15:30:49 +0300

    update prefix for moldbox css file to elemenate overlap

--- src/components/MoldBox/MoldBox.component.jsx
+++ src/components/MoldBox/MoldBox.component.jsx
@@ -5,23 +5,23 @@ export default function MoldBox({ number, status }) {
   let css = "";
   switch (status) {
     case "mounted":
-      css = "periodic-element mounted";
+      css = "moldbox-periodic-element moldbox-mounted";
       break;
 
     case "rejected":
-      css = "periodic-element rejected";
+      css = "moldbox-periodic-element moldbox-rejected";
       break;
 
     default:
-      css = "periodic-element not-mounted";
+      css = "moldbox-periodic-element moldbox-not-mounted";
   }
   return (
     <div
       className={css}
       // data-description="I'm only on here to reject invitations 😂"
     >
-      <div className="periodic-element-inner">
-        <div className="title">{number}</div>
+      <div className="moldbox-periodic-element-inner">
+        <div className="moldbox-title">{number}</div>
         {/* <div class="description">LinkedIn</div> */}
       </div>
     </div>
