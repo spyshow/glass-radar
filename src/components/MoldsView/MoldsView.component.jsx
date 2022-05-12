e01a85e34d5f5e9aee62acf683992a66ec4555a1
jihad khorfan <spyshow86@gmail.com>
2022-05-12 09:18:30 +0300

    add molds view component

---
+++ src/components/MoldsView/MoldsView.component.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { PageHeader, Button, Skeleton } from "antd";
+import { useGet } from "figbird";
+import { TableOutlined, AppstoreOutlined } from "@ant-design/icons";
+
+import MoldsTableView from "../../components/MoldsTableView/MoldsTableView.component";
+import MoldsCardView from "../../components/MoldsCardView/MoldsCardView.component";
+import "./MoldsView.styles.css";
+
+const MoldsView = ({ id }) => {
+  const [listView, setListView] = React.useState(false);
+  const changeViewToList = () => {
+    setListView(true);
+  };
+  const changeViewToCard = () => {
+    setListView(false);
+  };
+  const molds = useGet(id, "moldsets", {
+    realtime: "refetch",
+    fetchPolicy: "network-only",
+  });
+  if (molds.status !== "success") {
+    return <Skeleton active />;
+  }
+  return (
+    <div>
+      <PageHeader
+        ghost={true}
+        title="Mold Sets"
+        subTitle="Mold Sets Overview"
+        extra={[
+          <Button.Group key="15">
+            <Button
+              type="primary"
+              key="AppstoreOutlined"
+              onClick={changeViewToCard}
+            >
+              <AppstoreOutlined />
+            </Button>
+            <Button
+              type="primary"
+              key="TableOutlined"
+              onClick={changeViewToList}
+            >
+              <TableOutlined />
+            </Button>
+          </Button.Group>,
+        ]}
+      ></PageHeader>
+      <div className="mold-sets-container">
+        {listView ? (
+          <MoldsTableView molds={molds} />
+        ) : (
+          <MoldsCardView molds={molds} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MoldsView;
